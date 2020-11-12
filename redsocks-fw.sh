ff287b2bd826768ae8b2f4e0a1cedab77d2361d3
Simon Clow <simon.clow@contextis.com>
2020-11-12 14:32:22 +0000

    Updated firewall config for locally sourced traffic to be DNAT'd not traffic routing through container

--- redsocks-fw.sh
+++ redsocks-fw.sh
@@ -7,22 +7,31 @@ fw_setup() {
   # First we added a new chain called 'REDSOCKS' to the 'nat' table.
   iptables -t nat -N REDSOCKS
 
-  # Next we used "-j RETURN" rules for the networks we don’t want to use a proxy.
+  # Next we used "-j ACCEPT" rules for the networks we don't want to use a proxy.
   while read item; do
-      iptables -t nat -A REDSOCKS -d $item -j RETURN
+      iptables -t nat -A REDSOCKS -d $item -j ACCEPT
   done < /etc/redsocks-allowlist.txt
 
   # We then told iptables to redirect all port 80 connections to the http-relay redsocks port and all other connections to the http-connect redsocks port.
-  iptables -t nat -A REDSOCKS -p tcp --dport 80 -j REDIRECT --to-ports 12345
-  iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12346
+  #iptables -t nat -A REDSOCKS -p tcp --dport 80 -j REDIRECT --to-ports 12345
+  #iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12346
 
-  # Finally we tell iptables to use the ‘REDSOCKS’ chain for all outgoing connection in the network interface ‘$DOCKER_NET′.
+  # Finally we tell iptables to use the 'REDSOCKS' chain for all outgoing connection in the network interface '$DOCKER_NET'.
   if [ -z "$DOCKER_NET" ]
   then
     iptables -t nat -A PREROUTING -p tcp -j REDSOCKS
+    iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 12345
+    iptables -t nat -A PREROUTING -p tcp -j REDIRECT --to-ports 12346
   else
     iptables -t nat -A PREROUTING -i $DOCKER_NET -p tcp -j REDSOCKS
+    iptables -t nat -A PREROUTING -i $DOCKER_NET -p tcp --dport 80 -j REDIRECT --to-ports 12345
+    iptables -t nat -A PREROUTING -i $DOCKER_NET -p tcp -j REDIRECT --to-ports 12346
   fi
+
+  # Handle locally generated traffic (tinyproxy)
+  iptables -t nat -A OUTPUT -j REDSOCKS
+  iptables -t nat -A OUTPUT -p tcp --dport 80 -j DNAT --to-destination 127.0.0.1:12345
+  iptables -t nat -A OUTPUT -p tcp -j DNAT --to-destination 127.0.0.1:12346
 }
 
 ##########################
